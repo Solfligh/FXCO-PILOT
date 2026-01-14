bd8a93eaf9027546dda7929887382b97034430b7
Solfligh <xofligh28@gmail.com>
2026-01-14 02:53:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -576,4 +576,6 @@ Rules:
 # Run local server
 # --------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
